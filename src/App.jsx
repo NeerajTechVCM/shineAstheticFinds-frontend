f5802a0b4973a413c117bab015916360f51c7b35
Avinash <np6848586@gmail.com>
2025-05-20 13:39:12 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { useNavigate, useLocation } from "react-router-dom";
+import { useNavigate, useLocation, BrowserRouter as Router } from "react-router-dom";
 
 const AppWrapper = () => {
   const [user, setUser] = useState(null);
